13e14dd4421e36c9ccb198df6766f265470b4e14
kanatatsu64 <kanatatsu64@gmail.com>
2020-10-27 19:45:27 +0900

    add sytax diagram

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,11 @@
 module Main where
 
 {-
-    Cmd := <Label> = Expr1 | Expr1 ; Cmd
+    Cmd := Expr0 ; | Expr0 ; Cmd | Expr0
+    Expr0 := <Label> = Expr1 | Expr1
     Expr1 := Expr2 + Expr1 | Expr2 - Expr1 | Expr2
     Expr2 := Expr3 * Expr2 | Expr3 / Expr2 | Expr3
-    Expr3 := (Cmd) | <Label> | Value
-    Value := <Float>
+    Expr3 := (Cmd) | <Label> | <Value>
 
     > var = 1 + (2 + 3) * 4 ; var + 1.2
     22.2
